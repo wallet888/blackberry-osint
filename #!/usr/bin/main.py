0fcfcc58df79365d31bd591a99cdcd392cf3a245
wallet888 <edengotai06110@gmail.com>
2026-07-04 22:04:29 +0200

    Remove main script for BlackBerry OSINT

--- #!/usr/bin/main.py
+++ #!/usr/bin/main.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python3
-"""
-BlackBerry OSINT - Point d'entrée principal.
-Lance l'interface en ligne de commande.
-"""
-from cli import menu_principal
 
-if __name__ == "__main__":
-    menu_principal()
